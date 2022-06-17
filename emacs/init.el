508cd8c1287c16cff383cc8d12af3763aa69ab2d
Lorenzo Alluminio <lorenzo.alluminio@protonmail.com>
2022-06-17 15:27:32 +0200

    updates

--- emacs/init.el
+++ emacs/init.el
@@ -158,3 +158,82 @@
   ("j" text-scale-increase "in")
   ("k" text-scale-decrease "out")
   ("f" nil "finished" :exit t))
+
+;; Org Mode Configuration ------------------------------------------------------
+
+(defun efs/org-mode-setup ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (visual-line-mode 1))
+
+(defun efs/org-font-setup ()
+  ;; Replace list hyphen with dot
+  (font-lock-add-keywords 'org-mode
+                          '(("^ *\\([-]\\) "
+                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+  ;; Set faces for heading levels
+  (dolist (face '((org-level-1 . 1.2)
+                  (org-level-2 . 1.1)
+                  (org-level-3 . 1.05)
+                  (org-level-4 . 1.0)
+                  (org-level-5 . 1.1)
+                  (org-level-6 . 1.1)
+                  (org-level-7 . 1.1)
+                  (org-level-8 . 1.1))))
+    ;;(set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
+
+  ;; Ensure that anything that should be fixed-pitch in Org files appears that way
+  ;;(set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+  ;;(set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
+  ;;(set-face-attribute 'org-table nil   :inherit '(shadow fixed-pitch))
+  ;;(set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  ;;(set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  ;;(set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  ;;(set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch))
+  )
+
+(use-package org
+  :hook (org-mode . efs/org-mode-setup)
+  :config
+  (setq org-ellipsis " ▾")
+  (efs/org-font-setup))
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+(defun efs/org-mode-visual-fill ()
+  (setq visual-fill-column-width 100
+        visual-fill-column-center-text t)
+  (visual-fill-column-mode 1))
+
+(use-package visual-fill-column
+  :hook (org-mode . efs/org-mode-visual-fill))
+
+;; org-roam config
+
+(use-package org-roam
+  :custom
+  (org-roam-directory "~/cryptomator_mnts/zettelkasten")
+  ;;(org-roam-completion-everywhere t)
+  (org-roam-capture-templates
+   '(("d" "default" plain
+      "%?"
+      :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n")
+      :unnarrowed t)
+     ;("l" "programming language" plain
+     ; "* Characteristics\n\n- Family: %?\n- Inspired by: \n\n* Reference:\n\n"
+     ; :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n")
+     ;:unnarrowed t)
+   ))
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n i" . org-roam-node-insert)
+	 ;;:map org-mode-map
+         ;;("C-M-i" . completion-at-point)
+	 )
+  :config
+  (org-roam-setup))
