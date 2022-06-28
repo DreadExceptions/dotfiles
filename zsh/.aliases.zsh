41af32d3336c6f100aec5b980572f7d6860b6ccf
Lorenzo Alluminio <lorenzo.alluminio@protonmail.com>
2022-06-28 09:58:27 +0200

    updates

--- zsh/.aliases.zsh
+++ zsh/.aliases.zsh
@@ -36,8 +36,21 @@ cs() { cd "$1" && ls; }
 path2clip() { readlink -e "$1" | xclip -selection clipboard  }
 pformat() { echo "$@" | tr " \n" "-" | tr '[:upper:]' '[:lower:]' | sed 's/-$//' | xclip -sel clip }
 wdx-ls-verbose() {
-	for point in $(wdx ls | tr "\n" " "); do
-		ppath=$(wdx show "$point")
-		echo "$point -> $ppath"
-	done
+for point in $(wdx ls | tr "\n" " "); do
+	ppath=$(wdx show "$point")
+	echo "$point -> $ppath"
+done
 } 
+function pomo() {
+	arg1=$1
+	shift
+	args="$*"
+
+	min=${arg1:?Example: pomo 15 Take a break}
+	sec=$((min * 60))
+	msg="${args:?Example: pomo 15 Take a break}"
+
+	while true; do
+		date '+%H:%M' && sleep "${sec:?}" && notify-send -u critical -t 0 -a pomo "${msg:?}"
+	done
+}
