7c561a45fee197db98ad204c76dba549569c9d19
Lorenzo Alluminio <lorenzo.alluminio@protonmail.com>
2022-08-04 14:13:38 +0200

    updates to install

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Author: Lorenzo Alluminio
 # Installs my base dev / hacking environment
 # TODO:
-# - install emacs, alacritty and atuin from source
+# - add installation of emacs and alacritty from source
 #----------------------------------------
 
 # BE CAREFUL: CONTAINS UNTESTED CODE
@@ -21,7 +21,6 @@ sudo tlp start
 sudo apt install etckeeper
 sudo apt install stow
 sudo apt install source-highlight
-sudo apt install emacs
 
 # rust install
 export RUSTUP_HOME=/opt/rust/rustup 
@@ -30,8 +29,7 @@ sudo mkdir -p /opt/rust
 sudo chown $USER:$USER /opt/rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-# install alacritty and atuin
-cargo install alacritty
+# atuin
 cargo install atuin
 
 # vim
